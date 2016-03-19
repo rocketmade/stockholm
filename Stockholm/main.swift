1b28f382914c28a2c5de7477cbc0abcd1b822db9
Brandon Roth <brandon@rocketmade.com>
2016-03-18 23:35:26 -0600

    reading files and extracting data working

--- Stockholm/main.swift
+++ Stockholm/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+let fileManager = NSFileManager.defaultManager()
 
+
+let fileExtension = "mobileprovision"
+
+let path = NSString(string: "~/Library/MobileDevice/Provisioning Profiles").stringByExpandingTildeInPath
+let files = try! fileManager.contentsOfDirectoryAtPath(path).flatMap{NSURL(string: $0)}.filter{$0.pathExtension == fileExtension}
+
+//for file in files {
+var collection = [String]()
+for file in files {
+    let fullPath = NSString(string: path).stringByAppendingPathComponent(file.absoluteString)
+    let args = ["cms","-D","-i",fullPath]
+    let task = NSTask()
+    task.launchPath = "/usr/bin/security"
+    task.arguments = args
+    task.standardInput = NSPipe()
+    task.standardError = NSPipe()
+    
+    let pipe = NSPipe()
+    task.standardOutput = pipe
+    let handler = pipe.fileHandleForReading
+    task.launch()
+    
+    let data = handler.readDataToEndOfFile()
+    let hash = try! NSPropertyListSerialization.propertyListWithData(data, options: NSPropertyListReadOptions(), format: nil) as! NSDictionary
+    let uuid = hash["UUID"]!
+    let name = hash["Name"]!
+    let creationDate = hash["CreationDate"]!
+    let x = hash["AppIDName"]!
+    
+    collection.append("\(name) - \(uuid) - \(creationDate)")
+}
+
+
+for e in collection.sort() {
+    print(e)
+}
